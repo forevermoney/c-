585e161921fcd95dfaf7e106a609c44d9fbf3982
forevermoney <3185498161@qq.com>
2018-03-26 19:19:45 +0800

    二叉树

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <ctime>
 #include <unordered_set>
 
+/*-----------动态规划-----------*/
+/*----------单词拆分----------*/
 /*
    using namespace std;
 class ListNode{
@@ -57,6 +59,7 @@ public:
 */
 /*  ----------贪心算法---------*/
 /*-------------买卖股票的最佳时机----------*/
+/*
 using namespace std;
 class Solution {
 public:
@@ -64,7 +67,7 @@ public:
      * @param prices: Given an integer array
      * @return: Maximum profit
      */
-    int maxProfit(vector<int> &prices) {
+   /* int maxProfit(vector<int> &prices) {
         // write your code here
         if(prices.size() == 0){ //时刻注意数组越界
             return 0;
@@ -93,4 +96,54 @@ int main(){
     Solution *s=new Solution;
     cout<<s->maxProfit(a);
     return 0;
-}
+}*/;
+/*-----------二叉树----------*/
+/*---------前序和中序遍历构造二叉树---------*/
+/*
+ * 前序遍历的顺序为左根右
+ * 中序遍历的顺序为根左右
+ * 因此只需要寻找到前序遍历中和中序遍历首节点相等的点,即可按照相应的顺序恢复出原本的二叉树结构
+ * 分清完全二叉树,满二叉树的区别
+ * 关于二叉树的分类有很多变型,有三节点树,二叉查找树,红黑树等都需有一个具体的了解
+ */
+//  Definition of TreeNode:
+//TODO
+/*
+using namespace std;
+  class TreeNode {
+  public:
+      int val;
+      TreeNode *left, *right;
+      TreeNode(int val) {
+          this->val = val;
+          this->left = this->right = NULL;
+      }
+  };
+
+
+class Solution {
+public:
+    /**
+     * @param inorder: A list of integers that inorder traversal of a tree
+     * @param postorder: A list of integers that postorder traversal of a tree
+     * @return: Root of a tree
+     */
+ /*   TreeNode * buildTree(vector<int> &inorder, vector<int> &postorder) {
+        // write your code here
+        TreeNode *tn=new TreeNode(0);
+        int i;
+        if(tn==NULL){
+            return  NULL;
+        }
+        tn->val=inorder[0];
+        while(i<inorder.size()-1){
+            if(inorder[i]>inorder[i+1]){
+                tn->left->val=inorder.at(i+1);
+                tn=tn->left;
+            } else if(inorder[i]<inorder[i+1]){
+
+        }
+    }
+};
+*/
+
